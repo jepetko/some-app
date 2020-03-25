17a1a1d6cd4bc2902612534e8eb0833c73142706
Katarina Golbang <katarina.golbang@rbinternational.com>
2020-03-25 14:18:17 +0100

    try to inject instance from a library linked by npm link -> does not work

---
+++ src/app/main.ts
@@ -0,0 +1,19 @@
+import alias from 'module-alias';
+import path from 'path';
+import {Container, Inject} from 'typescript-ioc';
+
+// handle alias properly for the local development (ts-node-dev) and production (node)
+const extension = path.extname(process.mainModule.filename);
+alias.addAlias('@awesome-lib', path.join(process.cwd(), 'node_modules', 'some-lib', 'dist'));
+alias.addAlias('@awesome/app', (extension === '.ts') ? __dirname : `${process.cwd()}/build`);
+
+import {B} from '@awesome-lib/b';
+
+export class Main {
+
+    constructor(@Inject private b: B) {
+        console.info('Main injected b: ', b);
+    }
+}
+
+const main = Container.get(Main);
