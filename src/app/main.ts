f958fcd9a1ec80bc48bd34db9edf0cc3ae8827db
Katarina Golbang <katarina.golbang@rbinternational.com>
2020-03-25 15:14:15 +0100

    aliases work as well for NOT linked some-lib, as soon as some-lib is linked, there is issue resolving the dependency

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,13 @@
+import alias from 'module-alias';
+import path from 'path';
 import {Container, Inject} from 'typescript-ioc';
-import {B} from 'some-lib/dist/b';
+
+// handle alias properly for the local development (ts-node-dev) and production (node)
+const extension = path.extname(process.mainModule.filename);
+alias.addAlias('@awesome-lib', path.join(process.cwd(), 'node_modules', 'some-lib', 'dist'));
+alias.addAlias('@awesome/app', (extension === '.ts') ? __dirname : `${process.cwd()}/build`);
+
+import {B} from '@awesome-lib/b';
 
 export class Main {
 
