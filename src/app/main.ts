d658f49542100b009ffd5aeae13c332a63958dc6
Katarina Golbang <katarina.golbang@rbinternational.com>
2020-03-25 14:31:13 +0100

    get rid of aliases etc. to see whether the symbolic link itself is causing the issues (no typescript/alias etc. interference)

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,19 +1,11 @@
-import alias from 'module-alias';
-import path from 'path';
 import {Container, Inject} from 'typescript-ioc';
-
-// handle alias properly for the local development (ts-node-dev) and production (node)
-const extension = path.extname(process.mainModule.filename);
-alias.addAlias('@awesome-lib', path.join(process.cwd(), 'node_modules', 'some-lib', 'dist'));
-alias.addAlias('@awesome/app', (extension === '.ts') ? __dirname : `${process.cwd()}/build`);
-
-import {B} from '@awesome-lib/b';
+import {B} from 'some-lib/dist/b';
 
 export class Main {
 
     constructor(@Inject private b: B) {
-        console.info('Main injected b: ', b);
+        console.info('Main injected b: ', this.b);
     }
 }
 
-const main = Container.get(Main);
+Container.get(Main);
